767fb064ce78a2774324fbdfce175b7fd1e1ad42
Ilya <ilya.n@zimalab.com>
2025-07-17 18:35:01 +0500

    feat: add revalidate and dynamic url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,25 +2,25 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce'
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace, push } = useRouter();
   
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const { replace } = useRouter();
-  
-    console.log(term)
-    if(term) {
-      params.set('query', term)
-    } else {
-      params.delete('query')
-    }
-  
-    replace(`${pathname}?${params.toString()}`);
-  }
+  const handleSearch = useDebouncedCallback((term: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1')
+      if(term) {
+        params.set('query', term) 
+      } else {
+        params.delete('query')
+      }
+    
+      push(`${pathname}?${params.toString()}`);
+    }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
